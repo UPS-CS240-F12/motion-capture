13ac6277adf63e015059eedb81a394f5be34097d
Max Gobel <earthbound350@gmail.com>
2012-11-13 01:33:59 -0800

    Refactored Gesture Definition
    Gesture Definition and Player Assignment has been moved into the ViChar
    Controller Core, keeping the Gesture Framework seperate from exact
    gestures.

---
+++ Vi-Char Kinect Controller/Controller Core/ViCharPlayers.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GestureFramework;
+using Microsoft.Kinect;
+
+namespace Controller_Core
+{   
+    public class PlayerOne : Player
+    {
+        public PlayerOne()
+        {
+            this.playerGestures = PlayerHelpers.ProcessGestureList(new List<ViCharGestures> { ViCharGestures.Moving, ViCharGestures.Jumping });
+        }
+
+        public override GestureMap GetGestureMap()
+        {
+            GestureMap map = new GestureMap(this);
+
+            Gesture moving = new Gesture("moving", 1000, (int)ViCharGestures.Moving);
+            moving.Components.Add(new GestureComponent(JointType.HandRight, JointType.ElbowRight,
+                JointRelationship.AboveAndLeft, JointRelationship.AboveAndRight));
+            map.Items.Add(moving);
+
+            Gesture jumping = new Gesture("jumping", 1000, (int)ViCharGestures.Jumping);
+            jumping.Components.Add(new GestureComponent(JointType.HandRight, JointType.ShoulderRight,
+                JointRelationship.Below, JointRelationship.Above));
+            jumping.Components.Add(new GestureComponent(JointType.HandLeft, JointType.ShoulderLeft,
+                JointRelationship.Below, JointRelationship.Above));
+            map.Items.Add(jumping);
+
+            return map;
+        }
+    }
+
+    public class PlayerTwo : Player
+    {
+        public PlayerTwo()
+        {
+            this.playerGestures = PlayerHelpers.ProcessGestureList(new List<ViCharGestures> { ViCharGestures.Turning, ViCharGestures.Jumping });
+        }
+
+        public override GestureMap GetGestureMap()
+        {
+            GestureMap map = new GestureMap(this);
+
+            Gesture jumping = new Gesture("jumping", 1000, (int)ViCharGestures.Jumping);
+            jumping.Components.Add(new GestureComponent(JointType.HandRight, JointType.ShoulderRight,
+                JointRelationship.Below, JointRelationship.Above));
+            jumping.Components.Add(new GestureComponent(JointType.HandLeft, JointType.ShoulderLeft,
+                JointRelationship.Below, JointRelationship.Above));
+            map.Items.Add(jumping);
+
+            Gesture turning = new Gesture("turning", 1000, (int)ViCharGestures.Turning);
+            turning.Components.Add(new GestureComponent(JointType.HandLeft, JointType.ShoulderLeft,
+                JointRelationship.BelowAndLeft, JointRelationship.BelowAndRight));
+            map.Items.Add(turning);
+
+            return map;
+        }
+    }
+
+    public class PlayerHelpers
+    {
+        public static List<int> ProcessGestureList(List<ViCharGestures> gestures)
+        {
+            List<int> gestureIDs = new List<int>();
+
+            return gestureIDs;
+        }
+    }
+}
--- Vi-Char Kinect Controller/GestureFramework/GestureMap.cs
+++ Vi-Char Kinect Controller/GestureFramework/GestureMap.cs
@@ -8,7 +8,6 @@ namespace GestureFramework
     {
         public List<Gesture> Items;
         public int GestureResetTimeout;
-        public GestureType Type;
 
 
         public GestureMap(Player p)
@@ -24,32 +23,8 @@ namespace GestureFramework
             //    JointRelationship.EndingRelationship, JointRelationship.BeginningRelationship));
             //Items.Add(template);
 
-            Gesture moving = new Gesture("moving", 1000, GestureType.Moving);
-            moving.Components.Add(new GestureComponent(JointType.HandRight, JointType.ElbowRight, 
-                JointRelationship.AboveAndLeft, JointRelationship.AboveAndRight));
-            Items.Add(moving);
-
-            Gesture jumping = new Gesture("jumping", 1000, GestureType.Jumping);
-            jumping.Components.Add(new GestureComponent(JointType.HandRight, JointType.ShoulderRight,
-                JointRelationship.Below, JointRelationship.Above));
-            jumping.Components.Add(new GestureComponent(JointType.HandLeft, JointType.ShoulderLeft,
-                JointRelationship.Below, JointRelationship.Above));
-            Items.Add(jumping);
-
-            Gesture turning = new Gesture("turning", 1000, GestureType.Turning);
-            turning.Components.Add(new GestureComponent(JointType.HandLeft, JointType.ShoulderLeft, 
-                JointRelationship.BelowAndLeft, JointRelationship.BelowAndRight));
-            Items.Add(turning);
         }
 
         
-    }
-
-    public enum GestureType
-    {
-        Moving,
-        Jumping,
-        Turning,
-        None
     }
 }
