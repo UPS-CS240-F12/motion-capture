c68417a60fa4b33eb3cbe15722ff1770e58a698c
Matt Burke <matthewcburke@gmail.com>
2012-11-20 09:48:02 -0800

    Defined more gestures.

--- Vi-Char Kinect Controller/GestureFramework/GestureMap.cs
+++ Vi-Char Kinect Controller/GestureFramework/GestureMap.cs
@@ -25,10 +25,34 @@ namespace GestureFramework
             //Items.Add(template);
             
             Gesture template = new Gesture("pointing", 1000, GestureType.Pointing);
-            template.Components.Add(new GestureComponent(JointType.HandRight, JointType.Head, 
+            pointing.Components.Add(new GestureComponent(JointType.HandRight, JointType.Head, 
                 JointRelationship.FrontBelow, JointRelationship.BehindAbove));
+            Items.Add(pointing);
+            
+            Gesture template = new Gesture("tieLaces", 1000, GestureType.TieLaces);
+            tieLaces.Components.Add(new GestureComponent(JointType.HandRight, JointType.AnkleRight, 
+                JointRelationship.Below, JointRelationship.Above));
             Items.Add(template);
+            
+            Gesture template = new Gesture("disco", 1000, GestureType.Disco);
+            disco.Components.Add(new GestureComponent(JointType.HandRight, JointType.ShoulderRight, 
+                JointRelationship.FrontBelowAndLeft, JointRelationship.BehindAboveAndRight));
+            Items.Add(disco);
 
+            Gesture jumping = new Gesture("fastJump", 100, GestureType.FastJump);
+            fastJump.Components.Add(new GestureComponent(JointType.HandRight, JointType.ShoulderRight,
+                JointRelationship.Below, JointRelationship.Above));
+            fastJump.Components.Add(new GestureComponent(JointType.HandLeft, JointType.ShoulderLeft,
+                JointRelationship.Below, JointRelationship.Above));
+            Items.Add(fastJump);
+            
+            Gesture jumping = new Gesture("slowJump", 10000, GestureType.SlowJump);
+            slowJump.Components.Add(new GestureComponent(JointType.HandRight, JointType.ShoulderRight,
+                JointRelationship.Below, JointRelationship.Above));
+            slowJump.Components.Add(new GestureComponent(JointType.HandLeft, JointType.ShoulderLeft,
+                JointRelationship.Below, JointRelationship.Above));
+            Items.Add(slowJump);
+            
             Gesture moving = new Gesture("moving", 1000, GestureType.Moving);
             moving.Components.Add(new GestureComponent(JointType.HandRight, JointType.ElbowRight, 
                 JointRelationship.AboveAndLeft, JointRelationship.AboveAndRight));
@@ -56,6 +80,10 @@ namespace GestureFramework
         Jumping,
         Turning,
         Pointing,
+        TieLaces,
+        Disco,
+        FastJump,
+        SlowJump,
         None
     }
 }
